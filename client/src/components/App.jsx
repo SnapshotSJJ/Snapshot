8faa140ea026bf0b34af749b66b175259c525a97
iiffd <sml.hng@gmail.com>
2018-02-21 19:42:54 -0800

    Render navbar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import Navbar from './Navbar.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <Navbar />
       </div>
     );
   }
