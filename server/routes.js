3ce7d2fe7411c9f17732a92c325f4321e7b53bea
Jon <jcho4794@gmail.com>
2018-02-23 11:31:32 -0800

    building routes/api endpoints + controller functions

---
+++ server/routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const ctrl = require('./controller');
+
+router.put('/users/follow/:username', ctrl.users.followSingleUser);
+
+router.get('/users/all', ctrl.users.getFollowerList);
+
+router.post('/posts/upload/submit', ctrl.posts.submitNewPost);
+
+// router.post('/posts/upload/upload', ctrl.posts.storeNewPost);
+
+router.get('/posts/all', ctrl.posts.getAllPosts);
+
+router.get('/posts/followers', ctrl.posts.getFollowersPosts);
+
+router.post('/posts/comment/:postID', ctrl.posts.postSingleComment)
+
+router.put('/posts/like/:postID', ctrl.posts.likeSinglePost);
+
+router.put('/users/accept/:username', ctrl.users.acceptNewFollower);
+
+
+module.exports = router;
