7f92ec9583c82870d3aec4ec9e1bc6ab829ee5ac
iiffd <sml.hng@gmail.com>
2018-02-23 16:52:16 -0800

    Dump data on getAllPosts

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+const parser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(cors());
+app.use(parser.json());
 app.use(routes);
 
 module.exports = app;
