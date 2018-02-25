8e882a7ce2a20b683d4b5a29cb7554195288c46b
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-02-24 16:36:17 -0800

    Refactor app.js into index.js and merged conflicts

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const parser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist'));
@@ -13,8 +16,4 @@ app.listen(1337, () => {
   console.log('Hey! Listen!');
 });
 
-
-
-
-
-
+module.exports = app;
