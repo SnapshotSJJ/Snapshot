0a7a8940c99731457227b5e4f26888286cb73a96
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-05 16:07:24 -0800

    input field resets after comment submission

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -6,7 +6,7 @@ const Comments = (props) => {
       <div>
         {props.comments.map((comment, index) => <li key={index}>{comment.name}: {comment.text}</li>)}
         <div>
-          <input onChange={props.createComment} />
+          <input value={props.myComment} onChange={props.createComment} />
           <button onClick={props.postComment.bind(null, {userId: props.userId, postId: props.postId})}>Submit</button>
         </div>
       </div>
